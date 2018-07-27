20e77834de3995cbc51d365b93aa5cf32d3e58fa
Cody Robertson <codyw.robertson@gmail.com>
2018-07-27 15:35:39 -0600

    Added timestamp to chat messages
    Also italicized the connect/disconnect message

--- chat_js/chat_js.js
+++ chat_js/chat_js.js
@@ -8,20 +8,24 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    io.emit('user connect');
+    io.emit('user connect', getTimeStr());
 
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', getTimeStr() + " " + msg);
         // I think this might help with private messaging
         //socket.broadcast.emit(msg);
     });
 
     socket.on('disconnect', function(){
-        io.emit('user disconnect');
+        io.emit('user disconnect', getTimeStr());
     });
 });
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+function getTimeStr() {
+    return "(" + new Date().toLocaleString() + "):";
+}
 
