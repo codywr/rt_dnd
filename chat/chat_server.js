b9c2aa4d9cc70eb8c27763e9817849f3dc23ffb1
Cody Robertson <codyw.robertson@gmail.com>
2018-08-02 15:23:04 -0600

    Code cleanup and a bit more re-org
    * Using eslint to enforce airbnb-base style guide (with some minor mods)
    * Also moved the project files to the repo's root dir (rather in the chat
      sub-dir). This will facility puting all the little pieces together

--- chat/chat_server.js
+++ chat/chat_server.js
@@ -1,31 +1,30 @@
 
-var app = require ('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/chat_js.html');
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/chat_client.html`);
 });
 
-io.on('connection', function(socket){
-    io.emit('user connect', getTimeStr());
+io.on('connection', (socket) => {
+  io.emit('user connect', getTimeStr());
 
-    socket.on('chat message', function(msg){
-        io.emit('chat message', getTimeStr() + " " + msg);
-        // I think this might help with private messaging
-        //socket.broadcast.emit(msg);
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', `${getTimeStr()} ${msg}`);
+    // I think this might help with private messaging
+    // socket.broadcast.emit(msg)
+  });
 
-    socket.on('disconnect', function(){
-        io.emit('user disconnect', getTimeStr());
-    });
+  socket.on('disconnect', () => {
+    io.emit('user disconnect', getTimeStr());
+  });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+const server = http.listen(3000, () => {
+  console.log('Real Time D&D app listening on port %s', server.address().port);
 });
 
 function getTimeStr() {
-    return "(" + new Date().toLocaleString() + "):";
+  return `(${new Date().toLocaleString()}):`;
 }
-
--- chat_php/scripts/chat_php.js
+++ chat_php/scripts/chat_php.js
@@ -2,44 +2,40 @@
 
 // TODO: should all functions be nested under '$(document).ready();'?
 
-$(document).ready(function() {
-
-    //If user wants to end session
-    $("#exit").click(function() {
-        var exit = confirm("Are you sure you want to end the session?");
-        if(exit==true) {
-            window.location = 'chat.php?logout=true';
-        }
-    });
-
-    //If user sends a chat msg
-    $("#submitmsg").click(function() { //User clicked 'send'
-        var clientmsg = $("#usermsg").val(); //Grab msg
-        $.post("post.php", {text: clientmsg}); //Send msg
-        $("#usermsg").attr("value", ""); //Clear msg
-        return false;
-    });
-
-    //Load the file containing the chat log
-    function loadLog() {
-        var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-        $.ajax({
-            url: "chat_log.html",
-            cache: false, //Want a fresh chat history
-            success: function(html) {
-                $("#chatbox").html(html); //Insert chat log into the #chatbox div
-                //Auto-scroll
-                var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-                if(newscrollHeight > oldscrollHeight) {
-                    $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                }
-            },
-        });
+$(document).ready(() => {
+  // If user wants to end session
+  $('#exit').click(() => {
+    const exit = window.confirm('Are you sure you want to end the session?');
+    if (exit === true) {
+      window.location = 'chat.php?logout=true';
     }
+  });
 
-    //Reload chat history every 1 seconds
-    setInterval (loadLog, 1000);
+  // If user sends a chat msg
+  $('#submitmsg').click(() => { // User clicked 'send'
+    const clientmsg = $('#usermsg').val(); // Grab msg
+    $.post('post.php', { text: clientmsg }); // Send msg
+    $('#usermsg').attr('value', ''); // Clear msg
+    return false;
+  });
 
+  // Load the file containing the chat log
+  function loadLog() {
+    const oldscrollHeight = $('#chatbox').attr('scrollHeight') - 20; // Scroll height before the request
+    $.ajax({
+      url: 'chat_log.html',
+      cache: false, // Want a fresh chat history
+      success(html) {
+        $('#chatbox').html(html); // Insert chat log into the #chatbox div
+        // Auto-scroll
+        const newscrollHeight = $('#chatbox').attr('scrollHeight') - 20; // Scroll height after the request
+        if (newscrollHeight > oldscrollHeight) {
+          $('#chatbox').animate({ scrollTop: newscrollHeight }, 'normal'); // Autoscroll to bottom of div
+        }
+      },
+    });
+  }
+
+  // Reload chat history every 1 seconds
+  setInterval(loadLog, 1000);
 });
-
-
